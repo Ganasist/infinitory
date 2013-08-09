cd6e2ff9742ada67db37b2412535eecf545e14a0
Bob Toner <johntrichereau@gmail.com>
2013-08-09 14:54:15 +0200

    revert nav bar and revmoved collapsible element

--- app/controllers/institutes_controller.rb
+++ app/controllers/institutes_controller.rb
@@ -10,7 +10,7 @@ class InstitutesController < ApplicationController
         marker.title "#{institute.name}"
       end
     else
-      @institutes = Institute.order(updated_at: :desc).page(params[:page]).per_page(10)
+      @institutes = Institute.all.page(params[:page]).per_page(20)
     end
   end
 
