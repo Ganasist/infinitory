67a21b179b5f2bb3660e6f6aa95c7235ccea79b7
Bob Toner <johntrichereau@gmail.com>
2013-08-12 20:00:36 +0200

    Fixed Index and spacing

--- app/controllers/institutes_controller.rb
+++ app/controllers/institutes_controller.rb
@@ -10,7 +10,7 @@ class InstitutesController < ApplicationController
         marker.title "#{institute.name}"
       end
     else
-      @institutes = Institute.order(updated_at: :desc)
+      @institutes = Institute.order(updated_at: :desc).page(params[:page]).per_page(15)
       @departments = Department.count
       @labs = Lab.count
     end
