e53210dcbbb0e1d771917e99ed1a3883355c49fb
Bob Toner <johntrichereau@gmail.com>
2013-08-12 16:06:08 +0200

    Update URL regex

--- app/models/department.rb
+++ app/models/department.rb
@@ -8,7 +8,8 @@ class Department < ActiveRecord::Base
 
   validates_associated :institute
   validates :name, :address, presence: true
-  validates :url, :format =>{ :with => /((http|https):\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+).[a-z]{2,5}(:[0-9]{1,5})?(\/.)?/ix,
+
+  validates :url, :format =>{ :with => /\A^((http|https):\/\/)(([a-z0-9-\.]*)\.)?([a-z0-9-]+)\.([a-z]{2,5})(:[0-9]{1,5})?(\/)?$\z/ix,
   														:message => " is not valid" }
 
   validates :name, uniqueness: { scope: :institute_id, 
