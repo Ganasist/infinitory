29c150505f7eee31b44b107e49a00c964fce524e
Bob Toner <johntrichereau@gmail.com>
2013-08-24 14:32:35 +0200

    Update signup pathing and header links

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -3,7 +3,12 @@ class Lab < ActiveRecord::Base
 	belongs_to :department
 	belongs_to :institute
 
-  validates :group_leader_email, uniqueness: true, presence: true
-  validates_associated :institute
+	validates :group_leader, presence: true
+  validates :group_leader_email, uniqueness: true, presence: true,
+  					format: { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
+                     :message => 'Invalid e-mail! Please provide a valid e-mail address'}
+
+  validates_associated :institute, presence: true
+  validates_associated :department
 
 end
