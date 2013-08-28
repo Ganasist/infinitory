d49e06784b89f787339a51beddeb5d53f79ccb15
Bob Toner <johntrichereau@gmail.com>
2013-08-28 18:26:47 +0200

    Update authentication to include everything except index and show in application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,22 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :token_authenticatable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :lockable, :timeoutable
 
   belongs_to :lab
 
   before_validation :admin_cascade
 
   protected
+
   	def admin_cascade
   		if self.super_admin?
   			self.group_leader = true
   			self.admin 				= true
   		elsif self.group_leader?
-  			self.admin = true
+  			self.admin 				= true
   		end
   	end
 end
