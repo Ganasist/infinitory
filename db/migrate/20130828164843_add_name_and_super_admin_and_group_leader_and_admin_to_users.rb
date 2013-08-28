36bd3014a9aca4ee7d692df8c208d3db027349b1
Bob Toner <johntrichereau@gmail.com>
2013-08-28 19:22:53 +0200

    Update Devise migration to include confirmation, update header to have sign-in, sign-up, sign-out links depending on signed-in status

--- db/migrate/20130828164843_add_name_and_super_admin_and_group_leader_and_admin_to_users.rb
+++ db/migrate/20130828164843_add_name_and_super_admin_and_group_leader_and_admin_to_users.rb
@@ -1,5 +1,6 @@
-class AddSuperAdminAndGroupLeaderAndAdminToUsers < ActiveRecord::Migration
+class AddNameAndSuperAdminAndGroupLeaderAndAdminToUsers < ActiveRecord::Migration
   def change
+  	add_column :users, :name, :string
     add_column :users, :super_admin, :boolean, default: false
     add_column :users, :group_leader, :boolean, default: false
     add_column :users, :admin, :boolean, default: false
