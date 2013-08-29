6898aabfaa036dae916036aef089281321f61293
Bob Toner <johntrichereau@gmail.com>
2013-08-29 12:44:05 +0200

    Remove group_leader from Users, create new Group_leader model, start setting up new model validations

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -4,14 +4,15 @@ class Lab < ActiveRecord::Base
 	belongs_to :institute
 
 	has_many 	 :users, dependent: :destroy
-
-	validates :group_leader, :institute, presence: true
+	has_one		 :group_leader, dependent: :destroy
 
   # validates :email, uniqueness: true, presence: true,
   # 					format: { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
   #                    :message => 'Invalid e-mail! Please provide a valid e-mail address'}
 
   validates_presence_of :institute
+  validates_presence_of :group_leader
+
   validates_associated :department
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130828164843) do
+ActiveRecord::Schema.define(version: 20130829104130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,24 @@ ActiveRecord::Schema.define(version: 20130828164843) do
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
+  create_table "group_leaders", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "group_leaders", ["email"], name: "index_group_leaders_on_email", unique: true, using: :btree
+  add_index "group_leaders", ["reset_password_token"], name: "index_group_leaders_on_reset_password_token", unique: true, using: :btree
+
   create_table "institutes", force: true do |t|
     t.string   "name"
     t.float    "latitude"
@@ -73,6 +91,7 @@ ActiveRecord::Schema.define(version: 20130828164843) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "room"
+    t.string   "email"
   end
 
   add_index "labs", ["department_id"], name: "index_labs_on_department_id", using: :btree
@@ -104,12 +123,13 @@ ActiveRecord::Schema.define(version: 20130828164843) do
     t.datetime "updated_at"
     t.string   "name"
     t.boolean  "super_admin",            default: false
-    t.boolean  "group_leader",           default: false
     t.boolean  "admin",                  default: false
+    t.integer  "lab_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["lab_id"], name: "index_users_on_lab_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
