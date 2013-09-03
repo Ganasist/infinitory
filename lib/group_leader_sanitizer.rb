2631c80e227a24ce3cbb6a2f9b611ec998b0bd1f
Bob Toner <johntrichereau@gmail.com>
2013-09-03 19:01:21 +0200

    Fix Devise custom sanitizers so GL and Users sign in properly

--- lib/group_leader_sanitizer.rb
+++ lib/group_leader_sanitizer.rb
@@ -1,6 +1,15 @@
 class GroupLeader::ParameterSanitizer < Devise::ParameterSanitizer
     private
+    def sign_in
+      default_params.permit self.for(:sign_in)
+    end
+
+    def sign_up
+        default_params.permit(:first_name, :last_name, :email, :institute_name, :department_name,
+        											:password, :password_confirmation)
+    end
+
     def account_update
-        default_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+      default_params.permit self.for(:account_update)
     end
 end
--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -1,6 +1,15 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
     private
-    def account_update
+
+    def sign_in
+      default_params.permit self.for(:sign_in)
+    end
+
+    def sign_up
         default_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    end
+
+    def account_update
+      default_params.permit self.for(:account_update)
     end
 end
