12475f5e158b9ae279bd504e3eb08a39166c6e04
Bob Toner <johntrichereau@gmail.com>
2013-09-05 13:01:32 +0200

    Change User edit path and form to Devise 'edit_user_registraion_path'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show]
 
   def index
   end
 
   def show
+  	@lab = Lab.where(params[:lab_id])
   end
 
-  def edit
-  end
-
-  def update
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -20,8 +16,8 @@ class UsersController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def lab_params
-      params.require(:lab).permit(:email, :first_name, :last_name,
-      														:group_leader_id, :department_id, :institute_id, :approved)
-    end
+    # def user_params
+    #   params.require(:lab).permit(:email, :first_name, :last_name,
+    #   														:group_leader_id, :department_id, :institute_id, :approved)
+    # end
 end
