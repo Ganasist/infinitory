27739b0ea4587c38032aafeb0c30c5f524f6e083
Bob Toner <johntrichereau@gmail.com>
2013-09-09 12:50:31 +0200

    Begin Admin authentication implementation by adding Approved boolean to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907123452) do
+ActiveRecord::Schema.define(version: 20130909104315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,7 +99,6 @@ ActiveRecord::Schema.define(version: 20130907123452) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "lab_id"
-    t.boolean  "approved"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "role"
@@ -107,6 +106,7 @@ ActiveRecord::Schema.define(version: 20130907123452) do
     t.integer  "department_id"
     t.boolean  "superuser",              default: false
     t.string   "description"
+    t.boolean  "approved",               default: false, null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
