ec731095271f6cc6445b432a16121e692e5fd007
Bob Toner <johntrichereau@gmail.com>
2013-09-19 19:54:00 +0200

    Trim down code to still allow user signup, GL authorization, user email and confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Infinitory::Application.routes.draw do
 
   devise_for :users, :path => '', :path_names => { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
-                                  :controllers => { registrations: 'registrations',
-                                                    confirmations: 'confirmations' }
+                                  :controllers => { registrations: 'registrations' }
  
   resources :institutes, shallow: true do
     resources :departments
