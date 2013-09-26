45fa524916c635ad1ad58fb902c14c0e5ee9ebd3
Bob Toner <johntrichereau@gmail.com>
2013-09-26 12:54:26 +0200

    Install carrierwave backgrounder gem, update sidekiq.rb in initializers folder

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,3 +4,13 @@ require 'sidekiq/web'
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["john", "loislane"]
 end
+
+if Rails.env.production? || Rails.env.staging?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
+end
