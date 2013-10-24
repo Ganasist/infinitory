32d477bc5661cc83d3c0a24730dbeaf55245fdb1
Bob Toner <johntrichereau@gmail.com>
2013-10-24 15:12:56 +0200

    Minor updates

--- app/validators/OneLabOneGl.rb
+++
@@ -1,7 +0,0 @@
-class OneLabOneGl < ActiveModel::Validator
-  def validate(record)
-    unless record.name.starts_with? 'X'
-      record.errors[:name] << 'Need a name starting with X please!'
-    end
-  end
-end
--- spec/models/lab_spec.rb
+++ spec/models/lab_spec.rb
@@ -1,30 +1,21 @@
 require 'spec_helper'
 
 describe Lab do
-
-  it 'has a valid factory' do
-  	expect(create(:lab)).to be_valid
-  end
-
   it 'is invalid without an email address' do
-  	expect(build(:lab, email: "")).to have(1).errors_on(:email)
+          institute = Institute.new
+          lab = Lab.new(institute: institute)
+          expect(lab).to_not be_valid
   end
 
   it 'is invalid without an institute' do
-  	expect(build(:lab, institute_name: "")).to have(1).errors_on(:institute)
+          lab = Lab.new(email: "lab@lab.com")
+          expect(lab).to_not be_valid
   end
 
-  it 'is valid with one group leader'
-
   it 'is invalid without a group leader'
+
   it 'is invalid with more than one group leader'
 
-  it { should respond_to(:lab_name) }
-  it { should respond_to(:lab_email) }
-  it { should respond_to(:gl_count) }
-  it { should respond_to(:gl) }  
-  it { should respond_to(:size) }
-  it { should respond_to(:city) }
-  it { should respond_to(:location) }
+  it 'is valid with one group leader'
 
 end
