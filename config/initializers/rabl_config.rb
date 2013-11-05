6c6e1065f599ed0b9e606ace524380da3aef4f05
Bob Toner <johntrichereau@gmail.com>
2013-11-05 16:03:24 +0100

    Fix caching issue for now, RABL and Codeflower now much faster

--- config/initializers/rabl_config.rb
+++ config/initializers/rabl_config.rb
@@ -1,6 +1,6 @@
 Rabl.configure do |config|
 	config.cache_sources = Rails.env != 'development' # Defaults to false
-	config.cache_all_output = true
+	# config.cache_all_output = true
   config.include_json_root = false
   config.include_child_root = false
 end
