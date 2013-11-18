c2a521bd8e063203c370865175bc60ce0aaab490
Bob Toner <johntrichereau@gmail.com>
2013-11-18 16:44:19 +0100

    Fix retire emailing so that it does not lose lab id before sending

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,10 +51,11 @@ class UsersController < ApplicationController
   end
 
   def retire
+    @labtemp = @lab
     @user.retire
     if @user.save
       flash[:notice] = "#{ @user.fullname } has been retired. #{ undo_link }"
-      UserMailer.delay(retry: false).retire_email(@user.id, @lab.id)
+      UserMailer.delay(retry: false).retire_email(@user.id, @labtemp.id)
       @lab.id = nil
     else
       flash[:alert] = "#{ @user.fullname } couldn't be retired..."
