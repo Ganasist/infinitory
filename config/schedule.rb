0da90436bd83145e19be8765fe5caa9d45e95d71
Bob Toner <johntrichereau@gmail.com>
2013-11-18 15:36:32 +0100

    Remove sideiq scheduler gem, leave Whenever as is

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "#{path}/log/cron.log"
 
-every 1.day do
+every 2.minutes do
   command "rm '#{path}/log/development.log'"
   command "rm '#{path}/log/test.log'"
   runner "PaperTrail::Version.delete_all['created_at < ?', 1.week.ago]"
