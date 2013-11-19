acf4a6d7725f9d3fc05b7a5255a3ca83e6642279
Bob Toner <johntrichereau@gmail.com>
2013-11-19 17:47:11 +0100

    Add URL to reagents model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119133011) do
+ActiveRecord::Schema.define(version: 20131119164301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20131119133011) do
     t.hstore   "properties"
     t.integer  "lab_id"
     t.integer  "user_id"
+    t.string   "url"
   end
 
   add_index "reagents", ["lab_id"], name: "index_reagents_on_lab_id", using: :btree
