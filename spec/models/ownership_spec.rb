0374479eab80f4ea9ee2358d269fff82d0233b26
Bob Toner <johntrichereau@gmail.com>
2013-11-20 15:36:21 +0100

    Add validation for Ownership model on reagents and users plus specs and db specs

--- spec/models/ownership_spec.rb
+++ spec/models/ownership_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Ownership do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context 'relationships' do
+    expect_it { to belong_to(:user) }
+    expect_it { to belong_to(:reagent) }
+  end
+
+  context 'validations' do
+    expect_it { to validate_presence_of(:user) }
+    expect_it { to validate_presence_of(:reagent) }
+  end
+
+  context 'database columns' do
+    expect_it { to have_db_column(:device_id).of_type(:integer) }
+    expect_it { to have_db_column(:reagent_id).of_type(:integer) }
+    expect_it { to have_db_column(:user_id).of_type(:integer) }
+    expect_it { to have_db_column(:created_at).of_type(:datetime) }
+    expect_it { to have_db_column(:updated_at).of_type(:datetime) }
+  end
 end
