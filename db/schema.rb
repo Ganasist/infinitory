08ef4e8a39bb837f7057a9a52ca80ed6a3fbc105
Bob Toner <johntrichereau@gmail.com>
2013-11-22 20:07:46 +0100

    Update acts_as_taggable so it works, will figure out pg_search and it tomorrow

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122181503) do
+ActiveRecord::Schema.define(version: 20131122190228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 20131122181503) do
     t.string   "name",                                               null: false
     t.string   "category",                                           null: false
     t.string   "location"
-    t.decimal  "price",                                   precision: 9, scale: 2
+    t.decimal  "price",        precision: 9, scale: 2
     t.string   "serial"
     t.datetime "created_at"
     t.datetime "updated_at"
