b76b786bb3772c23b2f1d49f69633234a60ec088
Bob Toner <johntrichereau@gmail.com>
2013-11-23 16:34:29 +0100

    Minor updates to error handling

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,4 +5,6 @@ every 1.day, at: '15:00' do
   command "rm '#{path}/log/development.log'"
   command "rm '#{path}/log/test.log'"
   runner "PaperTrail::Version.delete_all['created_at < ?', 1.week.ago]"
+
+  command "curl #{path}/errors/404 > public/404.html && curl #{path}/errors/403 > public/403.html && curl #{path}/errors/503 > public/503.html"
 end
