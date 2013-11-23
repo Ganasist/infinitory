77c534a3ab29540835b7f5150f624f324d6781c3
Bob Toner <johntrichereau@gmail.com>
2013-11-23 02:49:25 +0100

    Change migration to add lock_version column to all models at once, update reagent form view to allow canceling properly

--- db/migrate/20131122163948_add_lock_version_to_reagents.rb
+++
@@ -1,5 +0,0 @@
-class AddLockVersionToReagents < ActiveRecord::Migration
-  def change
-    add_column :reagents, :lock_version, :integer, default: 0, null: false
-  end
-end
--- db/migrate/20131123013732_add_locking_column_to_models.rb
+++ db/migrate/20131123013732_add_locking_column_to_models.rb
@@ -1,5 +1,6 @@
-class AddLockingColumnToLabsAndDepartmentsAndInstitutes < ActiveRecord::Migration
+class AddLockingColumnToModels < ActiveRecord::Migration
   def change
+  	add_column :reagents, :lock_version, :integer, default: 0, null: false
   	add_column :labs, :lock_version, :integer, default: 0, null: false
     add_column :departments, :lock_version, :integer, default: 0, null: false
     add_column :institutes, :lock_version, :integer, default: 0, null: false
