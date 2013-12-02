19aff6367588b49ab967ce50ca22648b05de2484
Bob Toner <johntrichereau@gmail.com>
2013-12-02 11:30:37 +0100

    Add month and year picker to Reagents expiration calendar

--- app/assets/javascripts/reagents.js.coffee
+++ app/assets/javascripts/reagents.js.coffee
@@ -2,5 +2,7 @@ jQuery ->
 	$('#reagent_expiration').datepicker
 		dateFormat: 'yy-mm-dd',
 		minDate: 0,
+		changeMonth: true,
+		changeYear: true,
 		showOtherMonths: true,
 	  selectOtherMonths: true
--- app/assets/javascripts/reagents.js.js
+++ app/assets/javascripts/reagents.js.js
@@ -3,6 +3,8 @@ jQuery(function() {
   return $('#reagent_expiration').datepicker({
     dateFormat: 'yy-mm-dd',
     minDate: 0,
+    changeMonth: true,
+    changeYear: true,
     showOtherMonths: true
   }, {
     selectOtherMonths: true
