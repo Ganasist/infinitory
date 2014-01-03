74360deb84a432c5758c8faba41b0dd90fe81a55
Bob Toner <johntrichereau@gmail.com>
2014-01-03 20:36:41 +0100

    Remove counter cache from ownership - Users - reagents - devices for now, doesnt work

--- db/migrate/20140103131611_add_counter_cache_for_devices_and_reagents_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddCounterCacheForDevicesAndReagentsToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :reagents_count, :integer, default: 0
-  	add_column :users, :devices_count, :integer, default: 0
-  end
-end
