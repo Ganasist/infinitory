235d8202b6f68d9f405cbe63d859025b71f4ee2e
Bob Toner <johntrichereau@gmail.com>
2014-01-04 14:20:02 +0100

    Update redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 $redis = Redis.new
-REDIS = Redis.new(host: 'localhost', port: 6379)
+# REDIS = Redis.new(host: 'localhost', port: 6379)
 
 # ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+uri = URI.parse("redis://username:password@my.host:6389")
+REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
