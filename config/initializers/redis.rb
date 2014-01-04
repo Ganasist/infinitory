20f021b188a2f7fb1d7c68c8120e1183f8b0ea3b
Bob Toner <johntrichereau@gmail.com>
2014-01-04 14:22:12 +0100

    Apply custom REDISTOGO_URL to redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,5 @@ $redis = Redis.new
 # REDIS = Redis.new(host: 'localhost', port: 6379)
 
 # ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
-uri = URI.parse("redis://username:password@my.host:6389")
+uri = URI.parse("redis://redistogo:2abe6b292108ffc35ff96cbf8bf99fea@grideye.redistogo.com:9438/")
 REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
