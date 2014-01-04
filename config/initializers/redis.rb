5ddfae3ddf8bdbfa934a9a1f7383a179a9a2dc9b
Bob Toner <johntrichereau@gmail.com>
2014-01-04 14:32:34 +0100

    Revert redis.rb file back

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 $redis = Redis.new
-# REDIS = Redis.new(host: 'localhost', port: 6379)
+REDIS = Redis.new(host: 'localhost', port: 6379)
 
-# ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
-uri = URI.parse("redis://redistogo:2abe6b292108ffc35ff96cbf8bf99fea@grideye.redistogo.com:9438/")
-REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+# ENV["REDISTOGO_URL"] = 'redis://redistogo:2abe6b292108ffc35ff96cbf8bf99fea@grideye.redistogo.com:9438/'
+# uri = URI.parse(ENV["REDISTOGO_URL"])
+# REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
