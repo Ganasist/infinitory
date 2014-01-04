8351247565a460a04c64952a25c0c31482fb9680
Bob Toner <johntrichereau@gmail.com>
2014-01-04 14:14:18 +0100

    Minor update, make footer text smaller

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 $redis = Redis.new
+REDIS = Redis.new(host: 'localhost', port: 6379)
 
-ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+# ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
+# uri = URI.parse(ENV["REDISTOGO_URL"])
+# REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
