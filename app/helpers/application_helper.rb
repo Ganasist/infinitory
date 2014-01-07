c7174830f572ca86f0e6112ed913ecb7d4746605
Bob Toner <johntrichereau@gmail.com>
2014-01-07 17:56:55 +0100

    Update buttons to align to the right via the badging function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,19 +10,19 @@ module ApplicationHelper
 	
 	def badging(activity)
 		if (activity.key == "reagent.create") || (activity.key == "device.create")
-			raw('<span class="badge badge-1">+1</span>')
+			raw('<span class="badge badge-1 pull-right">+1</span>')
 		elsif (activity.key == "reagent.update") || (activity.key == "device.update")
-	  	raw('<span class="badge badge-2">+2</span>')
+	  	raw('<span class="badge badge-2 pull-right">+2</span>')
 	  elsif (activity.key == "reagent.delete") || (activity.key == "device.delete")
-	  	raw('<span class="badge badge-3">+3</span>')
+	  	raw('<span class="badge badge-3 pull-right">+3</span>')
 	  elsif (activity.key == "reagent.clone") || (activity.key == "device.clone")
-	   	raw('<span class="badge badge-4">+4</span>')
+	   	raw('<span class="badge badge-4 pull-right">+4</span>')
 	  elsif (activity.key == "user.feedback")
-	   	raw('<span class="badge badge-5">+5</span>')
+	   	raw('<span class="badge badge-5 pull-right">+5</span>')
 	  elsif (activity.key == "user.invitation")
-	   	raw('<span class="badge badge-10">+10</span>')
+	   	raw('<span class="badge badge-10 pull-right">+10</span>')
 	  elsif (activity.key == "user.gl_invitation")
-	   	raw('<span class="badge badge-25">+25</span>')
+	   	raw('<span class="badge badge-25 pull-right">+25</span>')
 	  end
 	end
 end
