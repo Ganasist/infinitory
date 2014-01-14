9e1fb0c2293e1e8776c98377c65f660539be0388
Bob Toner <johntrichereau@gmail.com>
2014-01-14 18:58:18 +0100

    Still trying to get Sidekiq working??

--- app/workers/reagent_expiration_worker.rb
+++ app/workers/reagent_expiration_worker.rb
@@ -1,14 +1,11 @@
 class ReagentExpirationWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
-  sidekiq_options retry: false, backtrace: true
+  # sidekiq_options retry: false, backtrace: true
   
-  recurrence backfill: true do
-    daily
-  end
+  recurrence { minutely.second_of_minute(0,15,30,45) }
 
   def perform
   	Reagent.expiration_notice
-  	PaperTrail::Version.delete_all['created_at > ?', 1.month.ago]
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,13 +8,10 @@ end
 if Rails.env.production? || Rails.env.staging?
   Sidekiq.configure_server do |config|
     config.redis = { url: ENV['REDIS_URL'] }
+    config.poll_interval = 15
   end
 
   Sidekiq.configure_client do |config|
     config.redis = { url: ENV['REDIS_URL'] }
   end
-
-  Sidekiq.configure_server do |config|
-	  config.poll_interval = 60
-	end
 end
