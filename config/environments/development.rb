10327ed4f01ebe741378d6fc7ea1aabb96b3c5b9
Bob Toner <johntrichereau@gmail.com>
2014-01-21 16:54:28 +0100

    Major front end color update, buttons better now too.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Infinitory::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = false
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
   # ENV["REDISTOGO_URL"] = 'redis://redistogo:2abe6b292108ffc35ff96cbf8bf99fea@grideye.redistogo.com:9438/'
