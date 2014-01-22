fa8784178e90105b8810fae00161abd5edc54868
Bob Toner <johntrichereau@gmail.com>
2014-01-22 15:58:51 +0100

    Minor commit

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 # $redis = Redis.new(host: 'localhost', port: 6379)
 # REDIS = Redis.new(host: 'localhost', port: 6379)
 
+ENV["REDISTOGO_URL"] = 'redis://redistogo:2abe6b292108ffc35ff96cbf8bf99fea@grideye.redistogo.com:9438/'
 uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
 $redis = REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
