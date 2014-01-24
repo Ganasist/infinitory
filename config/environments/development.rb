071ab06246d3c5af2ecc064275dab4e87161c36b
Bob Toner <johntrichereau@gmail.com>
2014-01-24 13:11:49 +0100

    Make default config for paperclip-->AWS uploads, remove redundancy from controllers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,13 @@ Infinitory::Application.configure do
   config.assets.debug = true
   config.serve_static_assets = true
 
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  # Paperclip.options[:command_path] = "/usr/local/bin/"
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
