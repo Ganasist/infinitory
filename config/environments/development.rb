20f4314a5609db7cc042fa966b9efd70727b6303
Bob Toner <johntrichereau@gmail.com>
2014-01-24 13:49:33 +0100

    Add path info to S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,7 @@ Infinitory::Application.configure do
   # Paperclip.options[:command_path] = "/usr/local/bin/"
   config.paperclip_defaults = {
     :storage => :s3,
+    :path => "/image/:id/:filename",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
