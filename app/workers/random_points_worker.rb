34d55dcf57c684c4d31cd0dd5a5150bf0ae39601
Bob Toner <johntrichereau@gmail.com>
2014-01-25 11:51:03 +0100

    Restore random points worker and reagent expiration worker

--- app/workers/random_points_worker.rb
+++ app/workers/random_points_worker.rb
@@ -1,15 +1,15 @@
-# class RandomPointsWorker
-#   include Sidekiq::Worker
-#   include Sidetiq::Schedulable
+class RandomPointsWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
 
-#   recurrence backfill: true do
-#     hourly
-#   end
+  recurrence backfill: true do
+    hourly
+  end
   
-#   def perform
-#     User.find_each do |t|
-#     	t.add_points(rand(20))
-#     	t.save
-#     end
-#   end
-# end
+  def perform
+    User.find_each do |t|
+    	t.add_points(rand(20))
+    	t.save
+    end
+  end
+end
