5cb3736324466fa58c9ce6b0b682cece4dea87b4
Bob Toner <johntrichereau@gmail.com>
2014-01-26 12:19:32 +0100

    minor update, create Profile too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
       redirect_to @user, status: :moved_permanently
     end
     @activities = PublicActivity::Activity.includes(:trackable, :owner).where(owner: @user).page(params[:page]).per_page(10)
-
     @comments = @user.comments.recent.page(params[:page]).per_page(10)
     respond_to do |format|
       format.html # index.html.erb
