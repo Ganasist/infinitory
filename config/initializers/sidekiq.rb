f171c5ea58e2449c764a073328ade099af2c6470
Bob Toner <johntrichereau@gmail.com>
2014-01-27 13:40:05 +0100

    Redis / Sidekiq updates, works well locally now

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,19 @@
 require 'sidekiq'
 require 'sidekiq/web'
 
+
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["john", "loislane"]
+end
+
+if Rails.env.development?
+	Sidekiq.configure_server do |config|
+	  config.redis = { :url => 'redis://localhost:6379/0', :namespace => "infinitory_#{Rails.env}" }
+	end
+
+	Sidekiq.configure_client do |config|
+	  config.redis = { :url => 'redis://localhost:6379/0', :namespace => "infinitory_#{Rails.env}" }
+	end
 end
 
 if Rails.env.production? || Rails.env.staging?
