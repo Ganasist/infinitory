ef21b6f3fa79b6eabe6c4878a4b3ed98d9f6fa87
Bob Toner <johntrichereau@gmail.com>
2014-01-27 14:09:32 +0100

    Minor update|

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,11 @@
 Paperclip::Attachment.default_options[:url]						 = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] 					 = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:storage]        = :s3
-
+Paperclip::Attachment.default_options[:s3_permissions] = :public_read
+Paperclip::Attachment.default_options[:s3_protocol]    = 'http'
+Paperclip::Attachment.default_options[:s3_options]     = { :server_side_encryption => 'AES256',
+																								           :storage_class => :reduced_redundancy,
+																								           :content_disposition => 'attachment' }
 if Rails.env.production?
 	Paperclip::Attachment.default_options[:s3_credentials] = { :bucket => ENV['S3_PRO_BUCKET_NAME'],
 																												     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
@@ -11,9 +15,3 @@ else
 																												     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 																												     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] }
 end
-
-Paperclip::Attachment.default_options[:s3_permissions] = :public_read
-Paperclip::Attachment.default_options[:s3_protocol]    = 'http'
-Paperclip::Attachment.default_options[:s3_options]     = { :server_side_encryption => 'AES256',
-																								           :storage_class => :reduced_redundancy,
-																								           :content_disposition => 'attachment' }
