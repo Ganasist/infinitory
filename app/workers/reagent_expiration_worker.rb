bc380ff8f4b9691bc49568be444a9f6e63b9a186
Bob Toner <johntrichereau@gmail.com>
2014-01-27 11:48:17 +0100

    Minor update

--- app/workers/reagent_expiration_worker.rb
+++ app/workers/reagent_expiration_worker.rb
@@ -1,14 +1,14 @@
-class ReagentExpirationWorker
-  include Sidekiq::Worker
-  include Sidetiq::Schedulable
+# class ReagentExpirationWorker
+#   include Sidekiq::Worker
+#   include Sidetiq::Schedulable
 
-  recurrence backfill: true do
-    daily
-  end
+#   recurrence backfill: true do
+#     daily
+#   end
 
-  def perform
-  	Reagent.expiration_notice
-  	Comment.destroy_all ['created_at < ?', 3.months.ago]
-  	# PublicActivity::Activity.destroy_all ['created_at < ?', 3.months.ago]
-  end
-end
+#   def perform
+#   	Reagent.expiration_notice
+#   	Comment.destroy_all ['created_at < ?', 3.months.ago]
+#   	# PublicActivity::Activity.destroy_all ['created_at < ?', 3.months.ago]
+#   end
+# end
