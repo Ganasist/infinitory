a8bc17370de8ebb7fc64e04021c9d6790aa5cbbf
Bob Toner <johntrichereau@gmail.com>
2014-01-27 13:52:29 +0100

    Try to incorporate dev / pro versions

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,9 +1,17 @@
 Paperclip::Attachment.default_options[:url]						 = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] 					 = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:storage]        = :s3
-Paperclip::Attachment.default_options[:s3_credentials] = { :bucket => ENV['S3_BUCKET_NAME'],
-																											     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-																											     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] }																											    
+
+if Rails.env.production?
+	Paperclip::Attachment.default_options[:s3_credentials] = { :bucket => ENV['S3_PRO_BUCKET_NAME'],
+																												     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+																												     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] }
+else
+	Paperclip::Attachment.default_options[:s3_credentials] = { :bucket => ENV['S3_DEV_BUCKET_NAME'],
+																												     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+																												     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] }
+end
+
 Paperclip::Attachment.default_options[:s3_permissions] = :public_read
 Paperclip::Attachment.default_options[:s3_protocol]    = 'http'
 Paperclip::Attachment.default_options[:s3_options]     = { :server_side_encryption => 'AES256',
