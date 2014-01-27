43535edef17c3ff7b878193a12aa48a07fd19345
Bob Toner <johntrichereau@gmail.com>
2014-01-27 18:53:15 +0100

    Minor update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,14 +5,14 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["john", "loislane"]
 end
 
-if Rails.env.development?
-	Sidekiq.configure_server do |config|
-	  config.redis = { :url => 'redis://localhost:6379/0', :namespace => "infinitory_#{Rails.env}" }
-	end
-	Sidekiq.configure_client do |config|
-	  config.redis = { :url => 'redis://localhost:6379/0', :namespace => "infinitory_#{Rails.env}" }
-	end
-end
+# if Rails.env.development?
+# 	Sidekiq.configure_server do |config|
+# 	  config.redis = { :url => 'redis://localhost:6379/0', :namespace => "infinitory_#{Rails.env}" }
+# 	end
+# 	Sidekiq.configure_client do |config|
+# 	  config.redis = { :url => 'redis://localhost:6379/0', :namespace => "infinitory_#{Rails.env}" }
+# 	end
+# end
 
 if Rails.env.production? || Rails.env.staging?
   Sidekiq.configure_server do |config|
