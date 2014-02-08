518788565322dbf1cb8d18a48f50a93cff9525d2
Bob Toner <johntrichereau@gmail.com>
2014-02-08 11:22:36 +0100

    Update Lab view and model for new originals only approach

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -15,7 +15,7 @@ class Lab < ActiveRecord::Base
   attr_accessor :delete_icon
   attr_reader :icon_remote_url
   before_validation { icon.clear if delete_icon == '1' }
-  has_attached_file :icon, styles: { thumb: '50x50>', portrait: '800x450>' }
+  has_attached_file :icon, styles: { thumb: '50x50>', original: '800x450>' }
   validates_attachment :icon, :size => { :in => 0..2.megabytes, message: 'Picture must be under 2 megabytes in size' }
   validates_attachment_content_type :icon, :content_type => /^image\/(png|gif|jpeg)/, :message => 'only (png/gif/jpeg) images'
   process_in_background :icon
