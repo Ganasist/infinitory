6200df38d65e18e6786026e197002108b2f29bc1
Bob Toner <johntrichereau@gmail.com>
2014-02-08 12:29:08 +0100

    Minor update to sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ require 'sidekiq'
 require 'sidekiq/web'
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-  [user, password] == ["john", "loislane"]
+  [user, password] == ['john', 'loislane']
 end
 
 # if Rails.env.development?
@@ -14,12 +14,12 @@ end
 # 	end
 # end
 
-if Rails.env.production? || Rails.env.staging?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2)}
-    config.poll_interval = 15
-  end
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2)}
-  end
+# if Rails.env.production? || Rails.env.staging?
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
+  config.poll_interval = 15
 end
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
+end
+# end
