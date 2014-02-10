908b73c1fe952fb52af3dffee28499dc494cd178
Bob Toner <johntrichereau@gmail.com>
2014-02-10 12:00:35 +0100

    Last image update, will check on heroku now

--- app/models/device.rb
+++ app/models/device.rb
@@ -27,7 +27,7 @@ class Device < ActiveRecord::Base
   attr_accessor :delete_icon
   attr_reader :icon_remote_url  
   before_validation { icon.clear if delete_icon == '1' }
-  has_attached_file :icon, styles: { thumb: '50x50>', original: '450x300>' }                   
+  has_attached_file :icon, styles: { thumb: '50x50>', original: '450x450>' }                   
   validates_attachment :icon, :size => { :in => 0..2.megabytes, message: 'Picture must be under 2 megabytes in size' }
   validates_attachment_content_type :icon,
                                     :content_type => /^image\/(png|gif|jpeg)/,
