541ea0e61d213046407f80401d962de066bf3fd9
Bob Toner <johntrichereau@gmail.com>
2014-02-11 16:32:07 +0100

    More email updates, trying to fix Devise emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Infinitory::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-
-  config.action_mailer.default_url_options = { :host => 'www.infinitory.com' }
+  config.action_mailer.default_url_options = { host: 'www.infinitory.com' }
+  
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',
     :port                 => '25',
