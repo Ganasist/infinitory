41b6545a594683292cac6804be936a78bf132b71
Bob Toner <johntrichereau@gmail.com>
2014-02-14 20:29:09 +0100

    Minor update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,23 +1,23 @@
 require 'sidekiq'
 require 'sidekiq/web'
 
-ENV["REDIS_URL"] ||= "redis://localhost:6379"
+# ENV["REDIS_URL"] ||= "redis://localhost:6379"
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ['john', 'loislane']
 end
 
-if Rails.env.development?
-	Sidekiq.configure_server do |config|
-	  config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
-	end
+# if Rails.env.development?
+# 	Sidekiq.configure_server do |config|
+# 	  config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
+# 	end
 	
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq'  }
-  end
-end
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq'  }
+#   end
+# end
 
-if Rails.env.production?
+# if Rails.env.production?
 	Sidekiq.configure_server do |config|
 	  config.redis = { url: ENV['REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
 	  config.poll_interval = 15
@@ -25,4 +25,4 @@ if Rails.env.production?
 	Sidekiq.configure_client do |config|
 	  config.redis = { url: ENV['REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
 	end
-end
+# end
