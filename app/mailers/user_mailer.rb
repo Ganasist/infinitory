6d896255b6bd6f2dde05ca580e2534a3f72fc052
Bob Toner <johntrichereau@gmail.com>
2014-02-18 18:45:50 +0100

    Minor update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -26,6 +26,6 @@ class UserMailer < ActionMailer::Base
 
   def feedback_email(feedback)
     @feedback = feedback
-    mail(from: @feedback.username, to: "feedback@infinitory.com", subject: "Feedback from #{ @feedback.email }")
+    mail(from: "#{ @feedback.username }", to: "feedback@infinitory.com", subject: "Feedback from #{ @feedback.email }")
   end
 end
