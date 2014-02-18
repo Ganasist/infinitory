bd49a2bd8789d1c883ee0b1cedfacbf157355a2a
Bob Toner <johntrichereau@gmail.com>
2014-02-18 18:02:42 +0100

    Minor update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,33 +1,32 @@
 class UserMailer < ActionMailer::Base
-  default from: 'admin@infinitory.com'
 
   def request_email(user_id, lab_id)
     @user = User.find(user_id)
     @lab   = Lab.find(lab_id)
-    mail(to: @lab.email, subject:  "#{@user.fullname} would like to join your lab")
+    mail(to: @lab.email, subject:  "#{ @user.fullname } would like to join your lab")
   end
 
   def welcome_email(user_id, lab_id)
     @user = User.find(user_id)
     @lab  = Lab.find(lab_id)
-    mail(to: @user.email, subject: "Welcome to the #{@lab.gl.fullname} lab")
+    mail(to: @user.email, subject: "Welcome to the #{ @lab.gl.fullname } lab")
   end
 
   def rejection_email(user_id, lab_id)
     @user = User.find(user_id)
     @lab  = Lab.find(lab_id)
-    mail(to: @user.email, subject: "You cannot join the #{@lab.gl.fullname} lab at this time")
+    mail(to: @user.email, subject: "You cannot join the #{ @lab.gl.fullname } lab at this time")
   end
 
   def retire_email(user_id, lab_id)
     @user = User.find(user_id)
     @lab  = Lab.find(lab_id)
-    mail(to: @user.email, subject: "Farewell from the #{@lab.gl.fullname} lab")
+    mail(to: @user.email, subject: "Farewell from the #{ @lab.gl.fullname } lab")
   end
 
   def feedback_email(feedback)
     @feedback = feedback
-    mail(to: "feedback@infinitory.com", subject: "Feedback from #{@feedback.email}")
+    mail(from: @feedback.email to: "feedback@infinitory.com", subject: "Feedback from #{ @feedback.email }")
     User.find_by(email: @feedback.email).add_points(5)
   end
 end
