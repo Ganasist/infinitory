174b279d4e630164c43dc9d516826462344fe582
Bob Toner <johntrichereau@gmail.com>
2014-02-18 19:39:30 +0100

    Minor update

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Infinitory::Application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'infinitory.com',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
