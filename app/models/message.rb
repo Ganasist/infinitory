09faa2065fd70911d974e7358b372431f17898b2
Bob Toner <johntrichereau@gmail.com>
2014-02-18 18:21:08 +0100

    Minor update

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,7 +9,7 @@ class Message
   
   validates_presence_of :email
   validates_presence_of :comment
-  validates_presence_of :user_name
+  validates_presence_of :username
   validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :comment, maximum: 223
 end
