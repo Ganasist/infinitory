ee99107085e12a9b0289bf0d358e327625656329
Bob Toner <johntrichereau@gmail.com>
2014-02-20 13:35:22 +0100

    Minor update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,14 @@ Infinitory::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net'}
+  config.action_mailer.default_url_options = { host: 'www.infinitory.com'}
+
+  ActionMailer::Base.smtp_settings = {
+          :address        => 'smtp.sendgrid.net',
+          :port           => '587',
+          :authentication => :plain,
+          :user_name      => ENV['SENDGRID_USERNAME'],
+          :password       => ENV['SENDGRID_PASSWORD'],
+          :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
