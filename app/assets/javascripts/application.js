088ee1c53f38dbc968c359067fd73c365361ee78
Bob Toner <johntrichereau@gmail.com>
2014-02-25 20:33:26 +0100

    Add modals to FAQ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,24 @@
 //= require best_in_place.purr
 //= require twitter/bootstrap
 //= require_tree
+
+
+$('#einstein_modal').modal({
+        backdrop: true,
+        keyboard: true,
+        show: false
+    }).css({
+        width: '320px',
+        height: '600px',
+        padding: '10px'
+    });
+
+$('#nerd_modal').modal({
+        backdrop: true,
+        keyboard: true,
+        show: false
+    }).css({
+        width: '280px',
+        height: '550px',
+        padding: '10px'
+    });
