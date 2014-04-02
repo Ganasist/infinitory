57a0bf8df52bad34e0a595d8028d590b06eeab6c
Bob Toner <johntrichereau@gmail.com>
2014-04-02 17:31:08 +0200

    MU

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
-//= require twitter/bootstrap
 //= require jquery
 //= require jquery.ui.all
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require best_in_place
 //= require best_in_place.purr
 //= require_tree
