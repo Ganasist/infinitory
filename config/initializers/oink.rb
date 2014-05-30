365401dd05df3e72f638d397965d4e4dca343d84
Bob Toner <johntrichereau@gmail.com>
2014-05-30 14:14:48 +0200

    MU

--- config/initializers/oink.rb
+++ config/initializers/oink.rb
@@ -1,5 +1,5 @@
-# if Rails.env.production?
-# 	Rails.application.middleware.use( Oink::Middleware, logger: Hodel3000CompliantLogger.new(STDOUT))
-# elsif Rails.env.development?
+if Rails.env.production?
+	Rails.application.middleware.use( Oink::Middleware, logger: Hodel3000CompliantLogger.new(STDOUT))
+elsif Rails.env.development?
 	Rails.application.middleware.use( Oink::Middleware, logger: Rails.logger)
-# end
+end
