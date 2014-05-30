19d6a33e10f06a634c16cb71f729bf41fdd220f7
Bob Toner <johntrichereau@gmail.com>
2014-05-30 13:42:13 +0200

    Add OINK logging to heroku

--- config/initializers/oink.rb
+++ config/initializers/oink.rb
@@ -1,1 +1,5 @@
-Rails.application.middleware.use Oink::Middleware
+if Rails.env.production?
+	Rails.application.middleware.use( Oink::Middleware, logger: Hodel3000CompliantLogger.new(STDOUT))
+elsif Rails.env.development?
+	Rails.application.middleware.use( Oink::Middleware, logger: Rails.logger )
+end
