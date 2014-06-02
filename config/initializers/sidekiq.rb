eae4c861a9f847516dc549f8278d2315bb37c89d
Bob Toner <johntrichereau@gmail.com>
2014-06-02 14:48:04 +0200

    Updates to Redis and Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,27 +1,24 @@
 require 'sidekiq'
 require 'sidekiq/web'
 
-# ENV["REDIS_URL"] ||= "redis://localhost:6379"
-
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ['john', 'loislane']
 end
 
-# if Rails.env.development?
+# if Rails.env.production?
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['LIVE_REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
+  config.poll_interval = 15
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['LIVE_REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
+end
+# elsif Rails.env.development? || Rails.env.staging?
 # 	Sidekiq.configure_server do |config|
-# 	  config.redis = { url: ENV["REDIS_URL"], size: (Sidekiq.options[:concurrency]), namespace: 'sidekiq' }
+# 	  config.redis = { url: ENV["STAGING_REDISTOGO_URL"], size: (Sidekiq.options[:concurrency]), namespace: 'sidekiq' }
 # 	end
-
 #   Sidekiq.configure_client do |config|
-#     config.redis = { url: ENV["REDIS_URL"], namespace: "infinitory_#{Rails.env}"  }
+#     config.redis = { url: ENV["STAGING_REDISTOGO_URL"], namespace: "infinitory_#{Rails.env}"  }
 #   end
-# else
-	Sidekiq.configure_server do |config|
-	  config.redis = { url: ENV['REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
-	  config.poll_interval = 15
-	end
-
-	Sidekiq.configure_client do |config|
-	  config.redis = { url: ENV['REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
-	end
 # end
