f1514719a17428422e8ff4fdec2a973cd115f9f8
Bob Toner <johntrichereau@gmail.com>
2014-06-03 12:06:12 +0200

    Increase Sidekiq concurrency to 12 in production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -13,21 +13,3 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['LIVE_REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
 end
-
-# if Rails.env.production?
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: ENV['LIVE_REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
-#   config.poll_interval = 15
-# end
-
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: ENV['LIVE_REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
-# end
-# elsif Rails.env.development? || Rails.env.staging?
-# 	Sidekiq.configure_server do |config|
-# 	  config.redis = { url: ENV["STAGING_REDISTOGO_URL"], size: (Sidekiq.options[:concurrency]), namespace: 'sidekiq' }
-# 	end
-#   Sidekiq.configure_client do |config|
-#     config.redis = { url: ENV["STAGING_REDISTOGO_URL"], namespace: "infinitory_#{Rails.env}"  }
-#   end
-# end
