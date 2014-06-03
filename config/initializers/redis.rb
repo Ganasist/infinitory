6ed27e1f9ef33f0941322a71f3ddd5aa67c7cddd
Bob Toner <johntrichereau@gmail.com>
2014-06-03 15:20:00 +0200

    MU

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,2 @@
-# if Rails.env.development?
-# 	REDIS = Redis.new(host: 'localhost', port: 6379)
-# elsif Rails.env.production?
-# if Rails.env.development? || Rails.env.staging?
-# 	uri = URI.parse(ENV["STAGING_REDISTOGO_URL"])
-# elsif Rails.env.production?
-	# uri = URI.parse(ENV["LIVE_REDISTOGO_URL"])
-# end
-
 uri = URI.parse(ENV["LIVE_REDISTOGO_URL"])	
 REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
