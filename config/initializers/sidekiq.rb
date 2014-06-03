209f211197765279ba525ec5827294411ddedb5d
Bob Toner <johntrichereau@gmail.com>
2014-06-03 13:11:48 +0200

    MU

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,10 +6,14 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['LIVE_REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
+  config.redis = { url: ENV['LIVE_REDISTOGO_URL'],
+  								size: (Sidekiq.options[:concurrency] + 2), 
+  					 namespace: "infinitory_#{Rails.env}"}
   config.poll_interval = 15
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['LIVE_REDISTOGO_URL'], size: (Sidekiq.options[:concurrency] + 2), namespace: "infinitory_#{Rails.env}"}
+  config.redis = { url: ENV['LIVE_REDISTOGO_URL'],
+  								size: (Sidekiq.options[:concurrency] + 2),
+  					 namespace: "infinitory_#{Rails.env}"}
 end
