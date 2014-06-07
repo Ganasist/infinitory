a067f79188006bedab267ec702dcf7996c7b6f86
Bob Toner <johntrichereau@gmail.com>
2014-06-07 15:01:27 +0200

    MU

---
+++ app/mailers/collaboration_mailer.rb
@@ -0,0 +1,10 @@
+class CollaborationMailer < ActionMailer::Base
+
+	# def request_email(email, department, institute)
+ #    @email  = email
+ #    @department = department
+ #    @institute = institute
+ #    mail(to: "feedback@infinitory.com", from: @email, subject: "Account request from #{ @email }" )
+ #  end
+
+end
