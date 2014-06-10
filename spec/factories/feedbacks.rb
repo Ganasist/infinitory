aba7666922b1bd8e35f7dfa19c62783a78829ef3
Bob Toner <johntrichereau@gmail.com>
2014-06-10 12:35:55 +0200

    MU to Feedback specs

--- spec/factories/feedbacks.rb
+++ spec/factories/feedbacks.rb
@@ -2,5 +2,8 @@
 
 FactoryGirl.define do
   factory :feedback do
+  	email									{ Faker::Internet.email }
+  	comment								{ Faker::Lorem.paragraph }
+  	user									"Bob Toner"
   end
 end
--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Feedback do
-  pending "add some examples to (or delete) #{__FILE__}"
+	# let(:feedback) { create(:feedback) }
+
+	context "validations" do
+		expect_it { to validate_presence_of(:email) }
+		expect_it { to allow_value('feedback@infinitory.com').for(:email) }
+		expect_it { to_not allow_value('asdfjkl').for(:email) }
+		expect_it { to validate_presence_of(:user) }
+		expect_it { to ensure_length_of(:comment).is_at_most(223) }
+		expect_it { to validate_presence_of(:comment).with_message("Feedback can't be blank") }
+	end
 end
