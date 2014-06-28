219caf6156c78941af315a9d799b928dc59e78b8
John Trichereau <johntrichereau@gmail.com>
2014-06-28 12:47:36 +0200

    Fix the inline checkboxes for sign-in and sign-up. Center the forgot password link on the sign-in dropdown

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -142,4 +142,16 @@ SimpleForm.setup do |config|
 
   # Cache SimpleForm inputs discovery
   # config.cache_discovery = !Rails.env.development?
+  config.wrappers :inline_checkbox, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.wrapper :tag => 'div', :class => 'controls' do |ba|
+      ba.wrapper :tag => 'label', :class => 'checkbox' do |bb|
+        bb.use :input
+        bb.use :label_text
+      end
+      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+    end
+  end
 end
